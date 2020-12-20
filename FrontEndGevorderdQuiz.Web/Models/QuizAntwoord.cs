fa3840a5918c607279226848373b52655bc9d934
David Urting <david.urting@live.com>
2020-12-20 13:21:23 +0100

    Add project files.

---
+++ FrontEndGevorderdQuiz.Web/Models/QuizAntwoord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEndGevorderdQuiz.Web.Models
+{
+    /// <summary>
+    /// Dit stelt één antwoord van de speler voor. 
+    /// </summary>
+    public class QuizAntwoord
+    {
+        /// <summary>
+        /// ID van de vraag waarvoor dit een antwoord van de speler is.
+        /// </summary>
+        public int VraagId { get; set; }
+
+        /// <summary>
+        /// Index van het gekozen antwoord (dus 0, 1, 2 of 3).
+        /// </summary>
+        public int GekozenAntwoordIndex { get; set; }
+    }
+}
